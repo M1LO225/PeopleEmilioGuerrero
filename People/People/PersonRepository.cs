f6e68ead0e684e2db06597704f943c0d964462f5
M1LO225 <emilio.guerrero.argoti@udla.edu.ec>
2025-01-07 12:58:29 -0500

    modelo person, poco de front

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using People.Models;
+﻿using SQLite;
+using People.Models;
 
 namespace People;
 
@@ -8,11 +9,15 @@ public class PersonRepository
 
     public string StatusMessage { get; set; }
 
-    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null) 
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -25,16 +30,16 @@ public class PersonRepository
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
-            // basic validation to ensure a name was entered
+
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
+            result = conn.Insert(new Person { Name = name });
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            
 
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            StatusMessage = string.Format("{0} Se ha añadido (Name: {1})", result, name);
         }
         catch (Exception ex)
         {
@@ -48,7 +53,8 @@ public class PersonRepository
         // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init ();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
